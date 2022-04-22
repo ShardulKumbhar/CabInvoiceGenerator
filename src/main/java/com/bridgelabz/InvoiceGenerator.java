729ba96e48e8d026bf6b4ce300eb813a03266ceb
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-22 12:51:29 +0530

    Step 1 calculating fare

---
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class InvoiceGenerator {
+    /**
+     * Created variables
+     */
+    private static final int MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final int MINIMUM_FARE = 5;
+
+    /**
+     *
+     * @param distance-value taking from user
+     * @param time-taking from user
+     * @return- fair by calculating
+     */
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
+    }
+
+
+}
