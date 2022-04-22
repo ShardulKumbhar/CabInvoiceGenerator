307318f2e7b3ca991ee77f3fe7a359af93540bd4
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-22 13:27:49 +0530

    Step 2 Calculating for Multiple Rides

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -9,17 +9,17 @@ public class InvoiceGenerator {
     private static final int MINIMUM_FARE = 5;
 
     /**
-     *
      * @param distance-value taking from user
-     * @param time-taking from user
+     * @param time-taking    from user
      * @return- fair by calculating
      */
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        /*
+        Checking if min fare less than 5 should give minimum 5 fare
+         */
         if (totalFare < MINIMUM_FARE)
             return MINIMUM_FARE;
         return totalFare;
     }
-
-
 }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -9,17 +9,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class InvoiceServiceTest {
     InvoiceGenerator invoiceGenerator;
 
+    /*
+      Step 1 Test case - Calculate fare
+     */
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         /**
-         * Calculating the fair
+         * Calculating the fare
          */
         double fare = invoiceGenerator.calculateFare(distance, time);
         assertEquals(25.0, fare, 0.0);
     }
 
-
+    /*
+     Step 2 Test Case for minimum fare should give 5
+     */
+    @Test
+    public void givenDistanceAndTime_WhenTotalFareLessThan10_ShouldReturnMinimumFare() {
+        invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        assertEquals(5.0, fare, 0.0);
+    }
 }
